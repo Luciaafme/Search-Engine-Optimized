ba462ecbda6ffab3b7c97c34906ec0d9768ee39a
Nestorpr <nesso2004@gmail.com>
2025-01-02 21:41:53 +0000

    Improvements on API Reports

--- QueryEngine/src/main/java/control/ApiServer2.java
+++ QueryEngine/src/main/java/control/ApiServer2.java
@@ -34,24 +34,40 @@ public class ApiServer2 {
 	}
 
 	public void configureRoutes() {
+
 		get("/search", (req, res) -> {
-			String phrase = req.queryParams("phrase");
-			String author = req.queryParams("author");
-			String startYear = req.queryParams("startyear");
-			String endYear = req.queryParams("endyear");
 
-			if (phrase == null || phrase.isEmpty()) {
-				res.status(400);
-				return gson.toJson(Map.of("error", "The search parameter 'phrase' is required"));
-			}
-
+			long start = System.currentTimeMillis();
 			try {
+
+				String phrase = req.queryParams("phrase");
+				String author = req.queryParams("author");
+				String startYear = req.queryParams("startyear");
+				String endYear = req.queryParams("endyear");
+
+
+				if (phrase == null || phrase.isEmpty()) {
+					res.status(400);
+					return gson.toJson(Map.of("error", "The search parameter 'phrase' is required"));
+				}
+
+
 				Map<String, Object> response = queryEngine.executeQuery(phrase, author, startYear, endYear);
 				res.type("application/json");
 				return gson.toJson(response);
+
 			} catch (Exception e) {
+
+				exceptions++;
 				res.status(500);
 				return gson.toJson(Map.of("error", "Intern error of the server", "details", e.getMessage()));
+			} finally {
+
+				long stop = System.currentTimeMillis();
+
+				synchronized (latencies) {
+					latencies.add((int) (stop - start));
+				}
 			}
 		});
 
@@ -89,6 +105,44 @@ public class ApiServer2 {
 			res.type("application/json");
 			return gson.toJson(results);
 		});
+
+		notFound((req, res) -> {
+			res.type("application/json");
+			return gson.toJson(Map.of("status", 404, "message", "Path not found"));
+		});
+
+		get("/report", (req, res) -> {
+
+			List<Integer> snapshotLatencies;
+			synchronized (latencies) {
+				snapshotLatencies = new ArrayList<>(latencies);
+			}
+
+			double avgResponseTime = snapshotLatencies.stream()
+					.mapToDouble(Integer::doubleValue)
+					.average()
+					.orElse(0);
+
+			int minLatency = snapshotLatencies.stream()
+					.min(Integer::compare)
+					.orElse(0);
+
+			int maxLatency = snapshotLatencies.stream()
+					.max(Integer::compare)
+					.orElse(0);
+
+			Map<String, Object> reportData = new HashMap<>();
+			reportData.put("timestamp", Instant.now().toString());
+			reportData.put("requests", snapshotLatencies.size());
+			reportData.put("avgResponseTime", avgResponseTime + " ms");
+			reportData.put("minLatency", minLatency + " ms");
+			reportData.put("maxLatency", maxLatency + " ms");
+			reportData.put("exceptions", exceptions);
+
+			res.type("application/json");
+			return gson.toJson(reportData);
+		});
+
 
 		notFound((req, res) -> {
 			res.type("application/json");
