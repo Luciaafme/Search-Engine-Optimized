b8cbb584d0a338f6a0bc7c35994028562e03a735
Nestorpr <nesso2004@gmail.com>
2025-01-07 19:26:54 +0000

    Cleaning API Report

--- QueryEngine/src/main/java/control/ApiServer2.java
+++ QueryEngine/src/main/java/control/ApiServer2.java
@@ -5,8 +5,10 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.Instant;
 import java.util.*;
+import org.jsoup.Jsoup;
 
 import static spark.Spark.*;
 
@@ -18,12 +20,17 @@ public class ApiServer2 {
 	private static final List<String> words = loadContent("C:\\Users\\alvar\\Desktop\\Universidad\\03 - AÑO3\\Semestre-1\\04-BD\\01-Teoria\\01-Assignments\\02-Individual Assignment\\04 - Stage4\\Search-Engine-Optimized\\QueryEngine\\src\\main\\resources\\words.txt");
 	private static final List<String> queries = loadContent("C:\\Users\\alvar\\Desktop\\Universidad\\03 - AÑO3\\Semestre-1\\04-BD\\01-Teoria\\01-Assignments\\02-Individual Assignment\\04 - Stage4\\Search-Engine-Optimized\\QueryEngine\\src\\main\\resources\\queries.txt");
 	private static final Random random = new Random();
+	private static int threads = 1;
+	private static int threadCount = 0;
+	private static long sleepTime = 100;
 	private static final List<Integer> latencies = new ArrayList<>();
 	private static int exceptions;
 	private QueryEngine queryEngine;
+	private static String baseUrl;
 
-	public ApiServer2(QueryEngine queryEngine) {
+	public ApiServer2(QueryEngine queryEngine, String baseUrl) {
 		this.queryEngine = queryEngine;
+		this.baseUrl = baseUrl;
 	}
 
 	public void configureRoutes() {
@@ -189,6 +196,104 @@ public class ApiServer2 {
 		});
 	}
 
+	private static TimerTask task() {
+		return new TimerTask() {
+			@Override
+			public void run() {
+				reportData(new ArrayList<>(latencies));
+				resetData();
+				increaseSpeed();
+			}
 
+			private void reportData(List<Integer> latencies) {
+				String sep = " | ";
+				String report = String.join(sep,
+						Instant.now() + "",
+						"Requests: " + latencies.size(),
+						"AVG response time: " + latencies.stream().mapToInt(i -> i).average().orElse(0) + " ms",
+						"Exceptions: " + exceptions,
+						"Threads: " + threadCount,
+						"Sleep time: " + sleepTime + " ms");
+
+				System.out.println("------- REPORT -------\n" + report);
+
+				saveReportToFile(report);
+			}
+
+			private static void saveReportToFile(String report) {
+				String filePath = "C:\\Users\\alvar\\Desktop\\Universidad\\03 - AÑO3\\Semestre-1\\04-BD\\01-Teoria\\01-Assignments\\02-Individual Assignment\\04 - Stage4\\Search-Engine-Optimized\\QueryEngine\\src\\main\\resources\\historialQueries.txt"; // Ruta del archivo donde se guardará el historial
+				//String filePath = "C:\\Users\\alvar\\Desktop\\Universidad\\03 - AÑO3\\Semestre-1\\04-BD\\01-Teoria\\01-Assignments\\02-Individual Assignment\\04 - Stage4\\Search-Engine-Optimized\\QueryEngine\\src\\main\\resources\\historialWords.txt"; // Ruta del archivo donde se guardará el historial
+
+				try {
+					Files.write(Paths.get(filePath), report.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+				} catch (IOException e) {
+					System.err.println("Error al escribir en el archivo: " + e.getMessage());
+				}
+			}
+
+
+
+			private void increaseSpeed() {
+				if (threadCount < 8) createThread();
+				else if (sleepTime > 2) sleepTime /= 2;
+			}
+
+			private void resetData() {
+				synchronized (latencies) {
+					latencies.clear();
+					exceptions = 0;
+				}
+			}
+		};
+	}
+
+
+	private static void createThread() {
+		new Thread(() -> {
+			threadCount++;
+			while (true) {
+				try {
+					doQuery();
+					Thread.sleep(sleepTime);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}).start();
+	}
+
+	private static void doQuery() {
+		try {
+			long start = System.currentTimeMillis();
+			//Jsoup.connect(baseUrl + "/test?phrase=" + words()).ignoreContentType(true).get(); // To test with only words
+			List<String> query = queries();
+			Jsoup.connect(baseUrl + "/test?phrase=" + query.get(0) + "?author=" + query.get(1) + "?yearStart=" + query.get(2) + "?yearEnd=" + query.get(3)).ignoreContentType(true).get(); // To test with queries with metadata
+
+			long stop = System.currentTimeMillis();
+			synchronized (latencies) {
+				latencies.add((int) (stop - start));
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			exceptions++;
+		}
+	}
+
+	private static String words() {
+		int count = new Random().nextInt(2) + 1;
+		StringBuilder result = new StringBuilder(words.get(new Random().nextInt(words.size())));
+		for (int i = 1; i < count; i++) result.append("&").append(words.get(new Random().nextInt(words.size())));
+		return result.toString();
+	}
+
+	private static List<String> queries() {
+		List<String> query = List.of(queries.get(random.nextInt(queries.size())).split(","));
+		return query;
+	}
+
+	public void execute() {
+		for (int i = 0; i < threads; i++) createThread();
+		new Timer().schedule(task(), 5000, 5000);
+	}
 }
 
